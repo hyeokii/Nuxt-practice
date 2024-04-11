efa7babd6a69d4916ea63fe2e62b7e259b7a3cc9
jieun <ccomo7071@gmail.com>
2024-04-11 16:27:39 +0900

    feat: 내가 쓴 todo 수정기능 추가

--- pages/todo/_id.vue
+++ pages/todo/_id.vue
@@ -1,26 +1,80 @@
 <template>
-
+  <div class="todoDetail">
+    <input
+      class="contentsInput"
+      v-model="editContents"
+      placeholder="수정할 컨텐츠"
+    />
+    <button class="saveBtn" @click="updateTodo">저장</button>
+  </div>
 </template>
+
 <script>
 export default {
-  name: 'todoDetail',
+  name: "todoDetail",
   props: {
     name: {
       type: String,
-      default: ''
-    }
+      default: "",
+    },
+    contents: {
+      type: String,
+      default: "",
+    },
   },
   data() {
-    return {}
+    return {
+      todo: {},
+      editContents: "",
+      message: "TodoList",
+    };
   },
-  async asyncData({$axios, route}) {
-    console.log(route.params.id)
+  async asyncData({ $axios, params }) {
+    const res = await $axios.get("http://localhost:3001/todoList");
+    const dataList = res.data;
+    const todoId = params.id;
+    const todo = dataList.find((todo) => todo.id === todoId);
+    return { todo };
   },
-  created() {
-
-  },
+  created() {},
   mounted() {
+    this.editContents = this.todo.contents;
+  },
 
-  }
-}
+  methods: {
+    async updateTodo() {
+      try {
+        const response = await this.$axios.put(
+          `http://localhost:3001/todoList/${this.todo.id}`,
+          {
+            ...this.todo,
+            contents: this.editContents,
+          }
+        );
+        if (response.status === 200) {
+          this.$router.push("/todo");
+        }
+      } catch (err) {
+        console.log("err", err);
+      }
+    },
+  },
+};
 </script>
+
+<style>
+.todoDetail {
+  background-color: white;
+  width: 300px;
+  height: 300px;
+}
+
+.contentsInput {
+  border: 1px solid black;
+}
+
+.saveBtn {
+  border: 1px solid black;
+  color: black;
+}
+</style>
--- pages/todo/index.vue
+++ pages/todo/index.vue
@@ -1,25 +1,39 @@
 <template>
   <div>
-    {{ message }}
+    {{ message }} <br />
+    <ul>
+      <li v-for="data in dataList" :key="data.id">
+        {{ data.contents }}
+        <NuxtLink :to="`/todo/${data.id}`">
+          <button v-if="curUserName === data.userNm">수정</button>
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
+
 <script>
+// 서버사이드
 
 export default {
-  name: 'MainPage',
+  name: "MainPage",
   data() {
     return {
       dataList: [],
-      message: "안녕하세요"
-    }
+      curUserName: "",
+      message: "TodoList",
+    };
   },
-  async asyncData({$axios}) {
-  },
-  created() {
-    console.log('data', this.dataList)
-  },
-  mounted() {
+  async asyncData({ $axios }) {
+    const todoData = await $axios.get("http://localhost:3001/todoList");
+    const usersRes = await $axios.get("http://localhost:3001/users");
+    const users = usersRes.data;
+    const curUserId = await $axios.get("http://localhost:3001/currentUser");
+    const curUser = users.find((user) => user.id === curUserId.data.id);
 
-  }
-}
+    return { dataList: todoData.data, curUserName: curUser.userNm };
+  },
+  created() {},
+  mounted() {},
+};
 </script>
