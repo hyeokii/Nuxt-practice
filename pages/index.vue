bccc457e28d3eb702351ef4b8d2f192d514d6057
김재혁 <re2volution@plateer.com>
2024-04-19 13:20:45 +0900

    초기 세팅

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hi</div>
+</template>
+<script>
+export default {
+  name: "MainPage",
+
+  data() {
+    return {
+      dataList: [],
+      message: "안녕하세요",
+      lastClicked: undefined,
+    };
+  },
+};
+</script>
