3c1e9a8f7af2ccb09c7c5321263133170c886034
김재혁 <re2volution@plateer.com>
2024-04-30 08:48:48 +0900

    mainswiper text 추가

--- components/MainSwiper.vue
+++ components/MainSwiper.vue
@@ -10,6 +10,20 @@
             class="swiper-image"
             :src="imageUrl(item.contentInfoList[0].imageList[0].pcContPathNm)"
           />
+          <div class="swiper-text">
+            <div
+              v-if="item.contentInfoList[1].textList[0]"
+              class="swiper-text1"
+            >
+              {{ item.contentInfoList[1].textList[0].contTitleNm }}
+            </div>
+            <div
+              v-if="item.contentInfoList[1].textList[1]"
+              class="swiper-text2"
+            >
+              {{ item.contentInfoList[1].textList[1].contTitleNm }}
+            </div>
+          </div>
         </div>
       </SwiperSlide>
     </Swiper>
@@ -99,6 +113,7 @@ export default {
       display: flex;
       justify-content: center;
       align-items: center;
+      position: relative;
       .swiper-link {
         cursor: pointer;
         width: 100%;
@@ -107,6 +122,23 @@ export default {
           width: 100%;
           height: 100%;
           object-fit: cover;
+        }
+        .swiper-text {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          font-weight: bold;
+          .swiper-text1 {
+            font-size: 1.875rem;
+          }
+          .swiper-text2 {
+            font-size: 1.25rem;
+          }
         }
       }
     }
