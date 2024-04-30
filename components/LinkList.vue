79e46a5d300d9469f531b4db4b63921a33e1da40
leesong <song@plateer.com>
2024-04-30 16:58:38 +0900

    dev - 기획전

---
+++ components/LinkList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="link-list">
+		<a href="javascript:void(0)">{{data.dispGrpNm}}</a>
+  </div>
+</template>
+<script>
+export default {
+  name: "LinkList",
+  props: {
+    
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {    
+  }
+};
+</script>
+<style scoped lang="scss">
+</style>
+
---
+++ pages/plan/index.vue
@@ -0,0 +1,17 @@
+<template>
+	 <div class="pg-plan">
+		<LinkList v-for="(corner, index) in planGroup" :key="index" />
+  </div>
+</template>
+<script>
+import {camelCase} from 'lodash'
+export default {
+	layout : 'Plan',
+	async asyncData({$axios}) {
+		const planGroupData = await $axios.get("https://gw.x2bee.com/api/display/v1/plan/group")
+		return {
+			planGroup : planGroupData?.data ?? []
+		}
+	}	
+}
+</script>
