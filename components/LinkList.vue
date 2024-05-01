d4cd0beb67031952a2178e8222b82a46514d9352
ssong <ssong911029@gmail.com>
2024-05-01 19:09:00 +0900

    모르겠음

--- components/LinkList.vue
+++ components/LinkList.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="link-list">
-		<a href="javascript:void(0)">{{data.dispGrpNm}}</a>
+    <template v-if="index === 0">
+      <button type="button" @click="addPlanData()">전체</button>
+    </template>
+		<button type="button" @click="addPlanData()">{{linkListData.dispGrpNm}}</button>
   </div>
 </template>
 <script>
 export default {
   name: "LinkList",
   props: {
-    
+    linkListData: {
+      type: Object,
+      defaultValue: undefined,
+    },
   },
   data() {
     return {};
   },
   created() {},
   mounted() {},
-  methods: {},
-  computed: {    
+  methods: {
+    addPlanData() {
+      this.$router.push({path:"/plan",query:{pageNo:1,groupNo: this.linkListData.dispGrpNo}});
+    }
+  },
+  computed: {
   }
 };
 </script>
--- pages/plan/index.vue
+++ pages/plan/index.vue
@@ -1,17 +1,33 @@
 <template>
 	 <div class="pg-plan">
-		<LinkList v-for="(corner, index) in planGroup" :key="index" />
+		<LinkList v-for="(linkListData, linkListIndex) in planGroup" :key="linkListIndex" :linkListData="linkListData"   />
   </div>
 </template>
 <script>
-import {camelCase} from 'lodash'
 export default {
 	layout : 'Plan',
-	async asyncData({$axios}) {
+	async asyncData({$axios,route}) {
+		console.log('11111')
 		const planGroupData = await $axios.get("https://gw.x2bee.com/api/display/v1/plan/group")
+		const planSearchData = {
+        dispMediaCd: "99",	// 전시매체코드(고정)
+        sortType: "recent",	// recent, close 정렬코드
+        pageNo: 1,	// 페이지 번호
+        pageSize: 9,	// 페이지사이즈 (고정)
+        progressYn: "Y",	// 진행중 여부
+        brandNo: "",	// 브랜드 번호
+        dispGrpNo: route.query.groupNo	// 기획전그룹번호 (없으면 전체)
+      };
+      const queryString = Object.entries(planSearchData).map(e => e.join('=')).join('&');
+      const planResultData = await $axios.get("https://gw.x2bee.com/api/display/v1/plan/planList?" + queryString);
+			console.log(planResultData)
+			console.log(route.query.groupNo)
+
 		return {
 			planGroup : planGroupData?.data ?? []
-		}
-	}	
+			
+		}	
+	}	,
+	computed : {}
 }
 </script>
